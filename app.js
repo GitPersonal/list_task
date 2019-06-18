b66fdaf1c86c2f89af7347344b934f90a71c211e
GitPersonal <juanchavez11@yahoo.com>
2019-06-18 16:16:33 -0500

    Update app.js
    add variables

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
 // Define UI vars
 const form = document.querySelector('#task-form');
 const taskList = document.querySelector('.collection');
+const cleatBtn = document.querySelector('.clear-tasks');
+const filter = document.querySelector('#filter');
+const taskInput = document.querySelector('task');
+
